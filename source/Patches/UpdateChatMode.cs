f3c0597678c7455a8d498138ce53bbf0b64979d5
fariparedes <you@example.com>
2023-09-03 14:29:30 -0400

    Fix Transporter and Glitch menus

---
+++ source/Patches/UpdateChatMode.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using HarmonyLib;
+using InnerNet;
+using TownOfUs.Roles;
+using UnityEngine;
+
+namespace TownOfUs.Patches
+{
+    [HarmonyPatch(typeof(ChatController), nameof(ChatController.UpdateChatMode))]
+    class UpdateChatMode
+    {
+        private static bool Prefix(ChatController __instance)
+        {
+            if (!PlayerControl.LocalPlayer.Is(RoleEnum.Glitch) && !PlayerControl.LocalPlayer.Is(RoleEnum.Transporter)) return true;
+            if (PlayerControl.LocalPlayer.Is(RoleEnum.Glitch))
+                return __instance != Role.GetRole<Glitch>(PlayerControl.LocalPlayer).MimicList;
+            if (Role.GetRole<Transporter>(PlayerControl.LocalPlayer).TransportList != null)
+                Debug.Log("Chat mode prefix " + (__instance != Role.GetRole<Transporter>(PlayerControl.LocalPlayer).TransportList).ToString());
+            return __instance != Role.GetRole<Transporter>(PlayerControl.LocalPlayer).TransportList;
+        }
+    }
+
+}
