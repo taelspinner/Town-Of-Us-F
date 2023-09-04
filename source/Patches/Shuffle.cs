75bc7d3d6e77831910af7ea7256c3d001e0ca25e
fariparedes <you@example.com>
2023-09-03 21:16:54 -0400

    Remove another unnecessary variable

--- source/Patches/Shuffle.cs
+++ source/Patches/Shuffle.cs
@@ -7,8 +7,7 @@ namespace TownOfUs
     {
         public static void Shuffle<T>(this List<T> list)
         {
-            var count = list.Count;
-            for (var i = count-1; i > 0; --i)
+            for (var i = list.Count-1; i > 0; --i)
             {
                 var j = Random.Range(0, i+1);
                 (list[i], list[j]) = (list[j], list[i]);
