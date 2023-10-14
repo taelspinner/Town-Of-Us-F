42210949dd5b6af020297b8941f184945316bde2
fariparedes <you@example.com>
2023-10-14 01:04:38 -0400

    Fix merge errors

--- source/Patches/Roles/Glitch.cs
+++ source/Patches/Roles/Glitch.cs
@@ -121,9 +121,7 @@ namespace TownOfUs.Roles
             }
 
             if (HudManager.Instance?.Chat != null)
-            {
                 foreach (var bubble in HudManager.Instance.Chat.chatBubblePool.activeChildren)
-                {
                     if (bubble.Cast<ChatBubble>().NameText != null &&
                         Player.Data.PlayerName == bubble.Cast<ChatBubble>().NameText.text)
                     {
