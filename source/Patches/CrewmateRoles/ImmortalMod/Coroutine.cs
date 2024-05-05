f7d3070c4d44209d3f3412e46ffb9eb0f797bbd0
fariparedes <you@example.com>
2024-05-05 05:43:30 -0400

    Too buggy, remove for now

--- source/Patches/CrewmateRoles/ImmortalMod/Coroutine.cs
+++ source/Patches/CrewmateRoles/ImmortalMod/Coroutine.cs
@@ -13,10 +13,10 @@ namespace TownOfUs.CrewmateRoles.ImmortalMod
     public class Coroutine
     {
 
-        public static IEnumerator ImmortalRevive(Immortal role, bool reviveNow = false)
+        public static IEnumerator ImmortalRevive(Immortal role)
         {
             var startTime = DateTime.UtcNow;
-            while (!reviveNow)
+            while (true)
             {
                 var now = DateTime.UtcNow;
                 var seconds = (now - startTime).TotalSeconds;
@@ -37,14 +37,11 @@ namespace TownOfUs.CrewmateRoles.ImmortalMod
                     deadBody.gameObject.Destroy();
                 }
             }
+            if (bodyPos == null) yield break;
 
+            Vector2 position = (Vector2)bodyPos;
             role.Player.Revive();
-            if (!MeetingHud.Instance)
-            {
-                if (bodyPos == null) yield break;
-                Vector2 position = (Vector2)bodyPos;
-                role.Player.NetTransform.SnapTo(new Vector2(position.x, position.y + 0.3636f));
-            }
+            role.Player.NetTransform.SnapTo(new Vector2(position.x, position.y + 0.3636f));
             RoleManager.Instance.SetRole(role.Player, RoleTypes.Crewmate);
             Murder.KilledPlayers.Remove(
                 Murder.KilledPlayers.FirstOrDefault(x => x.PlayerId == role.Player.PlayerId));
@@ -64,18 +61,6 @@ namespace TownOfUs.CrewmateRoles.ImmortalMod
             if (Patches.SubmergedCompatibility.isSubmerged() && PlayerControl.LocalPlayer.PlayerId == role.Player.PlayerId)
             {
                 Patches.SubmergedCompatibility.ChangeFloor(role.Player.transform.position.y > -7);
-            }
-
-            if(MeetingHud.Instance)
-            {
-                foreach(PlayerVoteArea voteArea in MeetingHud.Instance.playerStates)
-                {
-                    if (voteArea.TargetPlayerId == role.Player.PlayerId)
-                    {
-                        voteArea.AmDead = false;
-                        voteArea.XMark.gameObject.SetActive(false);
-                    }
-                }
             }
         }
     }
