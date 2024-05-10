3d10549d15a0f821e03deb1329e384cd09fc81da
fariparedes <you@example.com>
2024-05-10 00:10:50 -0400

    Fix light radius

--- source/Patches/NeutralRoles/FanaticMod/PerformKill.cs
+++ source/Patches/NeutralRoles/FanaticMod/PerformKill.cs
@@ -67,7 +67,7 @@ namespace TownOfUs.NeutralRoles.FanaticMod
             {
                 // indoctrinated player has to witness the kill to be converted
                 var switchSystem = GameOptionsManager.Instance.currentNormalGameOptions.MapId == 5 ? null : ShipStatus.Instance.Systems[SystemTypes.Electrical]?.TryCast<SwitchSystem>();
-                var lightRadius = switchSystem == null ? ShipStatus.Instance.MaxLightRadius : switchSystem.Value;
+                var lightRadius = switchSystem == null ? 1f : switchSystem.Value/255f;
                 var sightMod = GameOptionsManager.Instance.currentNormalGameOptions.CrewLightMod;
                 if (role.ConvertingPlayer.Is(RoleEnum.Glitch) ||
                     role.ConvertingPlayer.Is(RoleEnum.Juggernaut) || role.ConvertingPlayer.Is(RoleEnum.Pestilence) ||
@@ -76,7 +76,7 @@ namespace TownOfUs.NeutralRoles.FanaticMod
                     (role.ConvertingPlayer.Is(RoleEnum.Vampire) && CustomGameOptions.VampImpVision) ||
                     (role.ConvertingPlayer.Is(RoleEnum.Fanatic) && CustomGameOptions.FanaticImpVision))
                 {
-                    lightRadius = ShipStatus.Instance.MaxLightRadius;
+                    lightRadius = 1f;
                     sightMod = GameOptionsManager.Instance.currentNormalGameOptions.ImpostorLightMod;
                 }
                 else if (role.ConvertingPlayer.Is(RoleEnum.Werewolf))
@@ -84,7 +84,7 @@ namespace TownOfUs.NeutralRoles.FanaticMod
                     var ww = Role.GetRole<Werewolf>(role.ConvertingPlayer);
                     if (ww.Rampaged)
                     {
-                        lightRadius = ShipStatus.Instance.MaxLightRadius;
+                        lightRadius = 1f;
                         sightMod = GameOptionsManager.Instance.currentNormalGameOptions.ImpostorLightMod;
                     }
                 }
